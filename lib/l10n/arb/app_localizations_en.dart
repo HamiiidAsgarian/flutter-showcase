ecbe8dedf76aa93743dc205d8ae1a6e8d521aba4
Hamid Asgharian <hamiiid.asgarian@gmail.com>
2024-08-31 21:41:21 +0330

    update localization

--- lib/l10n/arb/app_localizations_en.dart
+++ lib/l10n/arb/app_localizations_en.dart
@@ -7,8 +7,29 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get settings => 'Settings';
+  String get onboardingTitle1 => 'Location and destination';
 
   @override
-  String get language => 'Language';
+  String get onboardingMessage1 =>
+      'Your destination is at your fingerprint.Open the app and enter where you want to go ';
+
+  @override
+  String get onboardingTitle2 => 'Services quality';
+
+  @override
+  String get onboardingMessage2 =>
+      'You have access to the best quality services ever';
+
+  @override
+  String get onboardingTitle3 => 'Be Comfortable';
+
+  @override
+  String get onboardingMessage3 =>
+      'Make sure that you will have a relax time with family';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get proceed => 'Proceed';
 }
--- lib/l10n/arb/app_localizations_ja.dart
+++ lib/l10n/arb/app_localizations_ja.dart
@@ -7,8 +7,29 @@ class AppLocalizationsJa extends AppLocalizations {
   AppLocalizationsJa([String locale = 'ja']) : super(locale);
 
   @override
-  String get settings => '設定';
+  String get onboardingTitle1 => 'Location and destination';
 
   @override
-  String get language => '言語';
+  String get onboardingMessage1 =>
+      'Your destination is at your fingerprint.Open the app and enter where you want to go ';
+
+  @override
+  String get onboardingTitle2 => 'Services quality';
+
+  @override
+  String get onboardingMessage2 =>
+      'You have access to the best quality services ever';
+
+  @override
+  String get onboardingTitle3 => 'Be Comfortable';
+
+  @override
+  String get onboardingMessage3 =>
+      'Make sure that you will have a relax time with family';
+
+  @override
+  String get next => '設定';
+
+  @override
+  String get proceed => '言語';
 }
