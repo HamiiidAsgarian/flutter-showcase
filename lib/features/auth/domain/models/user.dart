c48f41d8f5d2ff6ff233f44153f161000e15bcc1
Hamid Asgharian <hamiiid.asgarian@gmail.com>
2024-09-11 23:00:39 +0330

    add domain models

--- lib/features/auth/domain/models/user.dart
+++ lib/features/auth/domain/models/user.dart
@@ -1,6 +1,11 @@
-class User {
-  User({required this.id, required this.email, required this.password});
-  int id;
-  String email;
-  String password;
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  const User({required this.id, required this.email, required this.password});
+  final int? id;
+  final String email;
+  final String password;
+
+  @override
+  List<Object?> get props => [id, email, password];
 }
