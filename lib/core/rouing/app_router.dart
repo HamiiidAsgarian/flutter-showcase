9a1319d98024a472353c2dbb8313f1c3e7777a7d
Hamid Asgharian <hamiiid.asgarian@gmail.com>
2024-09-12 22:01:46 +0330

    Update routes, add bloc provider

--- lib/core/rouing/app_router.dart
+++ lib/core/rouing/app_router.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_showcase/features/auth/presentation/login_screen.dart';
+import 'package:flutter_showcase/features/auth/presentation/login_screen_bloc.dart';
+import 'package:flutter_showcase/features/auth/presentation/signup_screen.dart';
+import 'package:flutter_showcase/features/auth/presentation/signup_screen_bloc.dart';
+import 'package:flutter_showcase/features/onboarding/presentation/onboarding_bloc.dart';
 import 'package:flutter_showcase/features/onboarding/presentation/onboarding_screen.dart';
 import 'package:go_router/go_router.dart';
 
-enum Routes {
-  onBoarding,
-  login,
-}
+enum Routes { onBoarding, login, singup }
 
 class AppRouter {
   static GoRouter getRoutes() {
@@ -16,16 +17,26 @@ class AppRouter {
         GoRoute(
           path: '/onboarding',
           name: Routes.onBoarding.name,
-          builder: (BuildContext context, GoRouterState state) {
-            return const OnBoardingScreen();
-          },
+          builder: (context, state) => BlocProvider(
+            create: (context) => OnBoardingBloc(),
+            child: const OnBoardingScreen(),
+          ),
         ),
         GoRoute(
           path: '/login',
           name: Routes.login.name,
-          builder: (BuildContext context, GoRouterState state) {
-            return const LoginScreen();
-          },
+          builder: (context, state) => BlocProvider(
+            create: (context) => LoginScreeBloc(),
+            child: const LoginScreen(),
+          ),
+        ),
+        GoRoute(
+          path: '/signup',
+          name: Routes.singup.name,
+          builder: (context, state) => BlocProvider(
+            create: (context) => SignUpScreeBloc(),
+            child: const SignUpScreen(),
+          ),
         ),
       ],
     );
