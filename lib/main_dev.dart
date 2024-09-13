061dc2ae6bd7d691a0e362dda2318bc00aeb3663
Hamid Asgharian <hamiiid.asgarian@gmail.com>
2024-09-13 21:26:30 +0330

    Add environment variables

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_showcase/app_config.dart';
+import 'package:flutter_showcase/core/di/get_it.dart';
 import 'package:flutter_showcase/my_app.dart';
 
-///flutter run -t lib/main_prod.dart
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   AppConfig.appFlavor = Flavors.development;
+  await AppConfig.initialize();
+  await setupLocator();
+
   runApp(const MainApp());
 }
